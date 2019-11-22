a8ad53c19cdd99eba35de9ef7572c03b94cd2631
zhang <anran.zhang@dal.ca>
2019-11-22 03:41:32 -0400

    Added icons for the buses updates every 15sec

--- app/src/main/java/ca/dal/cs4176/azhang/bustracker/MapsActivity.kt
+++ app/src/main/java/ca/dal/cs4176/azhang/bustracker/MapsActivity.kt
@@ -5,22 +5,33 @@ import android.app.Activity
 import android.content.Context
 import android.content.SharedPreferences
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.location.Location
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.Menu
+import android.view.View
+import android.view.View.inflate
+import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.tasks.OnSuccessListener
@@ -31,11 +42,14 @@ import java.net.ConnectException
 import java.net.MalformedURLException
 import java.net.URL
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
-    companion object {
-        lateinit var mMap: GoogleMap
-    }
+
+    private lateinit var mMap: GoogleMap
+    lateinit var mainHandler: Handler
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var locationRequest:LocationRequest
+    private lateinit var locationCallback: LocationCallback
+    private var requestingLocationUpdates =false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
@@ -48,67 +62,62 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val button = findViewById<FloatingActionButton>(R.id.button)
         button.setOnClickListener {
             locate()
-            getData()
         }
+        mainHandler = Handler(Looper.getMainLooper())
     }
 
     override fun onStart() {
         super.onStart()
         if (!hasLocationPermissions()) {
+            //request location on first launch
             requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION), 35)
         } else {
             fusedLocationClient.lastLocation.addOnSuccessListener(this, OnSuccessListener {
                     location ->
-                if (location == null)
+                if (location == null) {
                     createLocationRequest()
+                    startLocationUpdates()
+                }
             }).addOnFailureListener(this) { e -> Log.w("FailLoc", "getLastLocation:onFailure", e)}
         }
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
         mMap.uiSettings.isZoomControlsEnabled = true
-        // Add a marker in Sydney and move the camera
-
+        mMap.uiSettings.isCompassEnabled = true
+        mMap.uiSettings.isMapToolbarEnabled=false
+        mMap.uiSettings.isRotateGesturesEnabled=true
+        val loc = LatLng(44.6392264,-63.5863118)
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, 14.0f))
+        getData()
     }
-    fun locate()
+
+    private fun locate()
     {
         if (this.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
             != PackageManager.PERMISSION_GRANTED) {
             // Permission is not yet granted, ask the user for permission
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), 0)
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), 35)
             //check the permission again
             if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED)
                 Toast.makeText(this, "Permission denied, can't get access to location ", Toast.LENGTH_LONG).show()
             else//permission is granted,proceed
-                LocUpdate()
+                locUpdate()
         }
-        else//else proceed directly
-            LocUpdate()
+        else
+            locUpdate()
     }
-     fun LocUpdate()
+     private fun locUpdate()
     {
         fusedLocationClient.lastLocation
             .addOnSuccessListener { location : Location? ->
-                // Got last known location.
-                Log.d("CORD",location.toString())//log the coordinates for debugging
-                if(location!=null) {//error check
-                    val latitude = location.latitude.toString()
-                    val longitude=location.longitude.toString()
-                    Log.d("CORD",latitude )
-                    Log.d("CORD", longitude)
-                    var loc = LatLng(location.latitude,location.longitude)
+                // Got last known location
+                if(location!=null) {
+                    Log.d("CORD",location.latitude.toString()+","+location.longitude)
+                    val loc = LatLng(location.latitude,location.longitude)
                     mMap.addMarker(MarkerOptions().position(loc))
-                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, 15.0f))
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, 16.0f))
                 }
                 else
                     Toast.makeText(this, "Failed to get location", Toast.LENGTH_SHORT).show()
@@ -119,18 +128,60 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     fun hasLocationPermissions() = this.checkSelfPermission(
         Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
     private fun createLocationRequest() {
-        val locationRequest = LocationRequest.create().apply {
+        locationRequest = LocationRequest.create().apply {
             interval = 10000
             fastestInterval = 5000
             priority = LocationRequest.PRIORITY_HIGH_ACCURACY
         }
     }
+    private val repeatingTask = object : Runnable {
+        override fun run() {
+            getData()
+            //This is to update the bus location every 15 seconds
+            mainHandler.postDelayed(this, 15000)
+        }
+    }
+
     fun getData()
     {
         var task = PositionReader().execute()
     }
+    private fun startLocationUpdates() {
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
+        requestingLocationUpdates = true
+    }
+    override fun onResume() {
+        super.onResume()
+        mainHandler.post(repeatingTask)
+        if (requestingLocationUpdates)
+            startLocationUpdates()
+    }
+    override fun onPause() {
+        super.onPause()
+        mainHandler.removeCallbacks(repeatingTask)
+        stopLocationUpdates()
+    }
+    private fun stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+        requestingLocationUpdates = false
+    }
+//Followed the answer on this post to figure out how to display the bus number in the marker properly https://stackoverflow.com/questions/24716987/using-an-xml-layout-for-a-google-map-marker-for-android
+    private fun createMaker(cord:LatLng,busNumber:String):MarkerOptions{
+        val marker:MarkerOptions = MarkerOptions()
+        marker.position(cord)
+        val view:View = inflate(this,R.layout.bus_marker_layout, null)
+        view.layoutParams=ViewGroup.LayoutParams(50, 50)
+        view.layout(50,50,50,50)
+        val b: Bitmap = Bitmap.createBitmap(50,50,Bitmap.Config.ARGB_8888)
+        val c = Canvas(b)
+        var busID = view.findViewById<TextView>(R.id.bus_id)
+        busID.text = busNumber
+        view.draw(c)
+        marker.icon(BitmapDescriptorFactory.fromBitmap(b))
+        return marker
+    }
 
-    //translated the Java code in the slides to Kotlin
+//translated the Java code in the slides to Kotlin
     inner class PositionReader : AsyncTask<Void,Void,List<FeedEntity>>(){
 
         private val url:URL = URL("http://gtfs.halifax.ca/realtime/Vehicle/VehiclePositions.pb")
@@ -145,18 +196,21 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             }
             catch (e:ConnectException)
             {
-                Log.e("DATA","Failed to connect")
+                Log.e("DATA","Failed to connect")//The app will somehow crash even if the exception is thrown so checking the connection before sending the request is the only way around this
                 throw e
             }
         }
         override
         fun onPostExecute(feedEntities:List<FeedEntity>) {
+            mMap.clear()
             for (entity:FeedEntity in feedEntities) {
                 //map updates
                 val routeID = entity.vehicle.trip.routeId
                 val loc = LatLng(entity.vehicle.position.latitude.toDouble(),entity.vehicle.position.longitude.toDouble())
-                mMap.addMarker(MarkerOptions().position(loc).title(routeID))
+                mMap.addMarker(MarkerOptions().position(loc).title(routeID).icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_bus)))
+                //mMap.addMarker(createMaker(loc,routeID))
             }
         }
     }
 }
+
